59e315049b4802508f4a68b13b8317b57a6d969b
ABHINAV THOMAS <abhinav@localhost.localdomain>
2016-08-01 14:25:19 +0530

    with file name as of original file

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var File = require('../models/file');
 var multer = require('multer');
-var upload = multer({dest:'public/uploads/'});
+//var upload = multer({dest:'public/uploads/'});
 // Get Homepage
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage });
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
@@ -33,24 +44,10 @@ router.get('/home', ensureAuthenticated, function (req, res, next) {
 });
 
 router.post('/home', upload.any(), ensureAuthenticated, function(req, res,next){
-	// var file = File();
-	// file.file=req.body.files;
-	// file.links=req.body.link;
-	// file.is_link=req.body.is_file;
-	
-	// file.save(function(err,file){
-	// 	if(err){
-	// 		next(err);
-	// 		return;
-	// }
 
 	req.flash('success_msg', 'Upload Finished');
 
 	res.redirect('/');
-
-	// res.send(req.body);
-	// res.send(req.files);
-
 	
 });
 
